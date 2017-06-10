ea7607ad118c79b334e9ee5584d5ef214059df66
distal-shores <samcoll@gmail.com>
2017-06-10 06:32:09 +0000

    Adding angled images, more home page elements.

--- htdocs/wp-content/themes/xradar/index.php
+++ htdocs/wp-content/themes/xradar/index.php
@@ -18,11 +18,37 @@ get_header(); ?>
 		<main id="main" class="site-main" role="main">
 			<div id="hero-photo">
 				<h1>Splash Tagline</h1>
-				<button>CTA Button</button>
+				<button class="cta-button">CTA Button</button>
 			</div>
+            <section id="company-intro">
+                <h1>Company Introduction</h1>
+                <p class="two-column">
+                    Keytar McSweeney's Williamsburg, readymade leggings try-hard 90's
+                    street art letterpress hoodie occupy Wes Anderson Banksy.
+                    Asymmetrical viral letterpress, McSweeney's seitan 3 wolf moon
+                    drinking vinegar sartorial pour-over messenger bag photo booth
+                    umami next level craft beer. Tote bag 8-bit flannel dreamcatcher photo
+                    booth. Tofu photo booth American Apparel, cliche brunch swag
+                    freegan lomo retro cray Vice gastropub post-ironic +1 pickled. Marfa
+                    Tonx next level, Banksy lo-fi vegan pop-up chia small batch DIY seitan
+                    asymmetrical viral. Disrupt salvia bespoke craft beer master cleanse.
+                    Butcher cornhole gentrify pop-up, raw denim viral literally.
+                    Keytar McSweeney's Williamsburg, readymade leggings try-hard 90's
+                    street art letterpress hoodie occupy Wes Anderson Banksy.
+                    Asymmetrical viral letterpress, McSweeney's seitan 3 wolf moon
+                    drinking vinegar sartorial pour-over messenger bag photo booth
+                    umami next level craft beer. Tote bag 8-bit flannel dreamcatcher photo
+                    booth. Tofu photo booth American Apparel, cliche brunch swag
+                    freegan lomo retro cray Vice gastropub post-ironic +1 pickled. Marfa
+                    Tonx next level, Banksy lo-fi vegan pop-up chia small batch DIY seitan
+                    asymmetrical viral. Disrupt salvia bespoke craft beer master cleanse.
+                    Butcher cornhole gentrify pop-up, raw denim viral literally.
+                </p>
+                <button class="cta-button">CTA Button</button>
+            </section>
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
 <?php
-get_sidebar();
+
 get_footer();
