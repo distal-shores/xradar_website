3f7c7b146bdf1f1ab7b399d003ebdb493500ef1e
Sam Coll <samcoll@gmail.com>
2017-07-07 17:22:36 -0700

    Finishing up the desktop view for the About page.

--- htdocs/wp-content/themes/xradar/about.php
+++ htdocs/wp-content/themes/xradar/about.php
@@ -12,81 +12,107 @@ get_header(); ?>
         <main id="main" class="page-about" role="main">
             <div id="hero-photo"></div>
             <section id="overview">
-                <h1>The most advanced GPR technology in the industry</h1>
+                <div class="white-box">
+                    <h1>The most advanced GPR technology in the industry</h1>
+                </div>
                 <hr>
-                <p>
-                    X-Radar is Canada’s leading provider of ground penetrating radar (GPR) technology, providing scanning services with our custom Xradar equipment. This newly designed technology has been developed specifically for scanning concrete, and has the accuracy of x-ray with the safety and efficiency of GPR. 
-                </p>
-                <p>
-                    With offices in Victoria, Vancouver, Toronto, Montreal, and Quebec City, we are ideally positioned to deliver services throughout all of Canada. We ensure the highest standard of service through our in house development program and unique training philosophy. Our dedicated team are fully equipped with the most advanced concrete scanning technology and are here to meet your needs.
-                </p>
-                <p>
-                    As pioneers of the industry and GPR’s practical application, we have always strived to innovate ways in which we can better serve our clients and the industry. None is more exemplary than our bespoke ​Xradar concrete scanning system. 
-                </p>
-                <p>
-                    This unique form of ground penetrating radar can be used for construction, engineering, archaeological applications, and more. Our employees collect and verify information on site, allowing you to make informed, timely and cost effective decisions, while ensuring safety.
-                </p>
+                <div class="two-column">
+                    <p>
+                        <strong>
+                            X-Radar is Canada’s leading provider of ground penetrating radar (GPR) technology, providing scanning services with our custom Xradar equipment. This newly designed technology has been developed specifically for scanning concrete, and has the accuracy of x-ray with the safety and efficiency of GPR.
+                        </strong>
+                    </p>
+                    <p>
+                        With offices in Victoria, Vancouver, Toronto, Montreal, and Quebec City, we are ideally positioned to deliver services throughout all of Canada. We ensure the highest standard of service through our in house development program and unique training philosophy. Our dedicated team are fully equipped with the most advanced concrete scanning technology and are here to meet your needs.
+                    </p>
+                    <p>
+                        As pioneers of the industry and GPR’s practical application, we have always strived to innovate ways in which we can better serve our clients and the industry. None is more exemplary than our bespoke ​Xradar concrete scanning system. 
+                    </p>
+                    <p>
+                        This unique form of ground penetrating radar can be used for construction, engineering, archaeological applications, and more. Our employees collect and verify information on site, allowing you to make informed, timely and cost effective decisions, while ensuring safety.
+                    </p>
+                </div>
             </section>
             <section id="why-xradar">
                 <h2>Why Xradar?</h2>
-                <ul class="grid-wrap">
-                    <li class="grid-item">
-                        <div class="why-icon leader"></div>
-                        Xradar is the leading provider of GPR and concrete scanning services in Canada. All of our technicians have undergone extensive proprietary training and are significantly experienced in the application of Xradar. We deliver to the highest industry standards and ensure the best results for our clients.
-                    </li>
-                    <li class="grid-item">
-                        <div class="why-icon radar"></div>
-                        We have developed the most advanced GPR technology in the form of Xradar, providing the accuracy of x-ray, with the safety and efficiency of GPR.
-                    </li>
-                    <li class="grid-item">
-                        <div class="why-icon canada"></div>
-                        We have offices all over Canada, including Victoria, Vancouver, Toronto, Montreal and Quebec, with more offices to be opened, meaning we are positioned to serve your needs wherever you are located.
-                    </li>
-                </ul>
+                <div class="three-column">
+                    <div class="column">
+                        <img src="http://xradar.dev/wp-content/uploads/2017/07/Trophy@1x.png"/>
+                        <h3>Industry Leader</h3>
+                        <hr>
+                        <p>Xradar is the leading provider of GPR and concrete scanning services in Canada. All of our technicians have undergone extensive proprietary training and are significantly experienced in the application of Xradar. We deliver to the highest industry standards and ensure the best results for our clients.</p>
+                    </div>
+                    <div class="column">
+                        <img src="http://xradar.dev/wp-content/uploads/2017/07/System-@1x.png"/>
+                        <h3>Advanced System</h3>
+                        <hr>
+                        <p>We have developed the most advanced GPR technology in the form of Xradar, providing the accuracy of x-ray, with the safety and efficiency of GPR.</p>
+                    </div>
+                    <div class="column">
+                        <img src="http://xradar.dev/wp-content/uploads/2017/07/Shape@1x.png"/>
+                        <h3>Nationwide</h3>
+                        <hr>
+                        <p>We have offices all over Canada, including Victoria, Vancouver, Toronto, Montreal and Quebec, with more offices to be opened, meaning we are positioned to serve your needs wherever you are located.</p>
+                    </div>
+                </div>
             </section>
             <div class="hero-image2"></div>
             <section id="methods">
                 <div class="method-explanation">
-                    <h2>Methods</h2>
-                    <hr>
-                    <p>
-                        We use our own bespoke Xradar technology, a form of ground penetrating radar (GPR) to carry out scanning services across a huge range of applications.
-                    </p>
-                    <div class="methods-photo"></div>
-                    <h3>What is Ground Penetrating Radar?</h3>
-                    <hr>
-                    <p>
-                        GPR is a non-destructive process used for scanning the subsurface for hidden objects. The process works by sending high-frequency electromagnetic waves into the ground from a transmitting antenna. These waves are then reflected back up to form an image of the subsurface. It can be used across a wide range of materials, including rock, soil, ice, fresh water, pavements and structures. GPR is able to detect subsurface features whose electrical properties contrast with those of the surrounding soil or concrete.
-                    </p>
-                    <div class="methods-photo"></div>
-                    <h3>The Process</h3>
-                    <hr>
-                    <p>
-                        Xradar’s system consists of a transmitter, a receiver, and a control unit. The transmitting antenna begins by sending electromagnetic waves into the ground, which are then reflected off underground layers and objects.
-                    </p>
-                    <p>
-                        Reflections are produced whenever the wave enters a material that differs in its conduction properties from the material it left. The receiving antenna then receives the wave reflections and measures the travel time and strength. This is then converted into an accurate image of whatever is below the surface.
-                    </p>
-                    <p>
-                        A number of factors can be altered depending on the requirement of the process. Changing the frequency of the wave can alter the depth and detail of the scan, therefore antenna choice is one of the most important factors in survey design.
-                    </p>
-                    <div class="methods-photo"></div>
-                    <h3>Applications</h3>
-                    <hr>
-                    <p>
-                        Xradar has applications in a number of fields. It can be used in earth sciences to study bedrock, soils and groundwater, along with water table profiling.
-                    </p>
-                    <p>
-                        Most commonly it can be used in engineering applications as a form of non-destructive testing (NDT) on structures and pavements. It can be used for concrete scanning, utility locating, and oil tank locating, to name a few.
-                    </p>
-                    <p>
-                        Another application lies in archaeology, with the detection and mapping of archaeological features and cemeteries. GPR also has uses in military and law enforcement, for the detection of dangerous objects such as landmines.
-                    </p>
+                    <div class="white-box">
+                        <h1>Method</h1>
+                        <hr>
+                        <p class="feature">
+                            We use our own bespoke Xradar technology, a form of ground penetrating radar (GPR) to carry out scanning services across a huge range of applications.
+                        </p>
+                    </div>
+                    <div class="picture">
+                        <div class="content">
+                            <h3>What is Ground Penetrating Radar?</h3>
+                            <hr>
+                            <p>
+                                GPR is a non-destructive process used for scanning the subsurface for hidden objects. The process works by sending high-frequency electromagnetic waves into the ground from a transmitting antenna. These waves are then reflected back up to form an image of the subsurface. It can be used across a wide range of materials, including rock, soil, ice, fresh water, pavements and structures. GPR is able to detect subsurface features whose electrical properties contrast with those of the surrounding soil or concrete.
+                            </p>
+                        </div>
+                        <div class="methods-photo hard-hat"></div>
+                    </div>
+                    <div class="picture left">
+                        <div class="methods-photo closeup"></div>
+                        <div class="content">
+                            <h3>The Process</h3>
+                            <hr>
+                            <p>
+                                Xradar’s system consists of a transmitter, a receiver, and a control unit. The transmitting antenna begins by sending electromagnetic waves into the ground, which are then reflected off underground layers and objects.
+                            </p>
+                            <p>
+                                Reflections are produced whenever the wave enters a material that differs in its conduction properties from the material it left. The receiving antenna then receives the wave reflections and measures the travel time and strength. This is then converted into an accurate image of whatever is below the surface.
+                            </p>
+                            <p>
+                                A number of factors can be altered depending on the requirement of the process. Changing the frequency of the wave can alter the depth and detail of the scan, therefore antenna choice is one of the most important factors in survey design.
+                            </p>
+                        </div>
+                    </div>
+                    <div class="picture">
+                        <div class="content">
+                            <h3>Applications</h3>
+                            <hr>
+                            <p>
+                                Xradar has applications in a number of fields. It can be used in earth sciences to study bedrock, soils and groundwater, along with water table profiling.
+                            </p>
+                            <p>
+                                Most commonly it can be used in engineering applications as a form of non-destructive testing (NDT) on structures and pavements. It can be used for concrete scanning, utility locating, and oil tank locating, to name a few.
+                            </p>
+                            <p>
+                                Another application lies in archaeology, with the detection and mapping of archaeological features and cemeteries. GPR also has uses in military and law enforcement, for the detection of dangerous objects such as landmines.
+                            </p>
+                        </div>
+                        <div class="methods-photo spray-paint"></div>
+                    </div>
                 </div>
             </section>
             <section id="know-more">
                 <h2>Want to know more?</h2>
-                <button class="white-btn">View Case Studies</button>
+                <button class="cta-button white">View Case Studies</button>
             </section>
         </main><!-- #main -->
     </div><!-- #primary -->
