714257dd619d6ca5996ec77f9a97220081522c4d
Sam Coll <samcoll@gmail.com>
2017-07-10 12:18:05 -0700

    Finishing up the layout and styling for Services page on Desktop

--- htdocs/wp-content/themes/xradar/services.php
+++ htdocs/wp-content/themes/xradar/services.php
@@ -11,49 +11,53 @@ get_header('services'); ?>
             <div id="hero-photo">
             </div>
             <section class="overview">
-                <h1>Our Services</h1>
-                <hr>
+                <div class="white-box">
+                    <h1>Our Services</h1>
+                    <hr>
+                </div>
                 <p>We offer a range of scanning services with a huge variety of applications, from construction, earth sciences, archaeology, to military. Using our own bespoke GPR technology, Xradar, we provide concrete scanning, concrete corrosion mapping, pulse echo, and engineering reporting services across all of Canada.</p>
                 <p>As leading service providers, we ensure the highest quality of delivery, guaranteeing your safety and saving you time and money. Using Xradar eliminates the accidental cutting and drilling through embedded objects and the high costs required for repair. It is the safest and most cost effective way to locate objects in concrete structures prior to cutting, coring or drilling.</p>
                 <p>Our offices in Victoria, Vancouver, Toronto, Montreal and Quebec City are perfectly placed to serve the needs of clients across the country.</p>
                 <p>Find out more about our services below:</p>
             </section>
-            <section class="concrete-scanning">
-                <div class="services-photo closeup"></div>
-                <div class="inner">
-                    <h2>Xradar Enhanced Concrete Scanning</h2>
-                    <hr>
-                    <p>​Prevent injuries, save time, money and headaches by choosing to locate and map the position of post tension cables, voids, rebar and electrical conduits embedded in concrete.</p>
-                    <a href="#">Read More</a>
-                </div>
-            </section>
-            <section class="corrosion-mapping">
-                <div class="services-photo spray-down"></div>
-                <div class="inner">
-                    <h2>Concrete Corrosion Mapping</h2>
-                    <hr>
-                    <p>Using ​Computer-Assisted image analysis of Xradar we are able to process and determine the corrosion of concrete.</p>
-                    <a href="#">Read More</a>
-                </div>
-            </section>
-            <section class="pulse-echo">
-                <div class="services-photo computer"></div>
-                <div class="inner">
-                    <h2>Pulse Echo</h2>
-                    <hr>
-                    <p>The use of Pulse Echo is a scanning technique ideal for thick concrete structures outside the limitation of GPR in isolation.</p>
-                    <a href="#">Read More</a>
-                </div>
-            </section>
-            <section class="engineering-reports">
-                <div class="services-photo port-mann"></div>
-                <div class="inner">
-                    <h2>Engineering Reports</h2>
-                    <hr>
-                    <p>Make informed decisions without the need for a site visit. Using our bespoke reporting services, we provide you with all the information needed to make decisions off-site allowing you to save time and keep projects moving.</p>
-                    <a href="#">Read More</a>
-                </div>
-            </section>
+            <div class="flex-wrap">
+                <section class="concrete-scanning">
+                    <div class="services-photo closeup"></div>
+                    <div class="inner">
+                        <h2>Xradar Enhanced Concrete Scanning</h2>
+                        <hr>
+                        <p>​Prevent injuries, save time, money and headaches by choosing to locate and map the position of post tension cables, voids, rebar and electrical conduits embedded in concrete.</p>
+                        <a href="#">Read More</a>
+                    </div>
+                </section>
+                <section class="corrosion-mapping">
+                    <div class="services-photo spray-down"></div>
+                    <div class="inner">
+                        <h2>Concrete Corrosion Mapping</h2>
+                        <hr>
+                        <p>Using ​Computer-Assisted image analysis of Xradar we are able to process and determine the corrosion of concrete.</p>
+                        <a href="#">Read More</a>
+                    </div>
+                </section>
+                <section class="pulse-echo">
+                    <div class="services-photo computer"></div>
+                    <div class="inner">
+                        <h2>Pulse Echo</h2>
+                        <hr>
+                        <p>The use of Pulse Echo is a scanning technique ideal for thick concrete structures outside the limitation of GPR in isolation.</p>
+                        <a href="#">Read More</a>
+                    </div>
+                </section>
+                <section class="engineering-reports">
+                    <div class="services-photo port-mann"></div>
+                    <div class="inner">
+                        <h2>Engineering Reports</h2>
+                        <hr>
+                        <p>Make informed decisions without the need for a site visit. Using our bespoke reporting services, we provide you with all the information needed to make decisions off-site allowing you to save time and keep projects moving.</p>
+                        <a href="#">Read More</a>
+                    </div>
+                </section>
+            </div>
             <section class="know-more">
                 <h2>See Our Services In Action</h2>
                 <button class="cta-button white">Learn More</button>
