b4cb8f531893bbd7fb1311ab1fb67baff9f44c68
Sam Coll <samcoll@gmail.com>
2017-07-11 15:58:38 -0700

    Updating the generic page header so it doesn't look like ass.

--- htdocs/wp-content/themes/xradar/header-page.php
+++ htdocs/wp-content/themes/xradar/header-page.php
@@ -13,7 +13,7 @@
 <div id="top-buffer">1-855-5-Xradar(97-2327)</div>
 <div id="page" class="site">
     <a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'xradar' ); ?></a>
-    <header id="masthead" class="site-header" role="banner">
+    <header id="masthead" class="site-header page" role="banner">
         <div class="site-branding">
             <img class="logo" src="http://xradar.dev/wp-content/uploads/2017/07/xr_logo_dark.png">
         </div>
