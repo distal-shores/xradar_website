5ec523637457fe400c937b188965c44e23ab2f3c
Sam Coll <samcoll@gmail.com>
2017-07-18 11:03:25 -0700

    Styling the case studies sub for tablet.

--- htdocs/wp-content/themes/xradar/single.php
+++ htdocs/wp-content/themes/xradar/single.php
@@ -19,11 +19,6 @@ get_header(); ?>
 
 			the_post_navigation();
 
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
-
 		endwhile; // End of the loop.
 		?>
 
@@ -33,3 +28,9 @@ get_header(); ?>
 <?php
 get_sidebar();
 get_footer();
+
+
+// If comments are open or we have at least one comment, load up the comment template.
+// if ( comments_open() || get_comments_number() ) :
+	// comments_template();
+// endif;
